f29f8c62b7a9d56d062c3b767906e1bab19f6453
Ricardo Lopes <paulo.lopes@iteris.com.br>
2022-12-20 15:17:46 -0300

    build: project ms-client created.

---
+++ projeto-cursoms/ms-client/src/test/java/br/com/rlopes/msclient/MsClientApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.rlopes.msclient;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class MsClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
