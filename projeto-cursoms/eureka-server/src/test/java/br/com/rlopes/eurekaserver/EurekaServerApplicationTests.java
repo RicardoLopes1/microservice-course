d71ed22074f76ca2894f2b4ee862c7fea2d579bb
Ricardo Lopes <paulo.lopes@iteris.com.br>
2022-12-20 14:09:00 -0300

    build: project eureka-server created.

---
+++ projeto-cursoms/eureka-server/src/test/java/br/com/rlopes/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.rlopes.eurekaserver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
